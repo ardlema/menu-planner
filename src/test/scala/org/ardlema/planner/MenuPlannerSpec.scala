54e055b04d32171bf43227d99b69f77f11efac42
Alberto Rodriguez <arodriguez@stratio.com>
2017-02-21 08:11:15 +0100

    Move MenuPlanner to its own class

--- src/test/scala/org/ardlema/planner/MenuPlannerSpec.scala
+++ src/test/scala/org/ardlema/planner/MenuPlannerSpec.scala
@@ -1,7 +1,5 @@
 package org.ardlema.planner
 
-import scala.util._
-
 import java.io.File
 
 import org.ardlema.parser.{Dish, DishParserFromTextFile}
@@ -45,29 +43,3 @@ class MenuPlannerSpec extends FlatSpec with Matchers {
 
   //TODO: Wrong path tests!!
 }
-
-object MenuPlanner {
-
-  def planAWeek(lunches: List[Dish], typesPerDay: List[(WeekDay, DishType)]) = {
-
-    def dishesForAWeek(
-        lunchesToBeSelected: List[Dish],
-        typesPerDay: List[(WeekDay, DishType)],
-        lunchesSelected: List[(WeekDay, Dish)]): List[(WeekDay, Dish)] = {
-      if (lunchesSelected.size == WeekDays.weekDays.size) lunchesSelected
-      else {
-        val random = Random
-        val filteredDishes = lunchesToBeSelected.filter(d => d.dishType.identifier.equals(typesPerDay.head._2.identifier))
-        val nextDish = random.nextInt(filteredDishes.size)
-        val elementSelected = filteredDishes(nextDish)
-        val newLunchesToBeSelected = lunchesToBeSelected.filterNot(d => d.description.equals(elementSelected.description))
-        dishesForAWeek(newLunchesToBeSelected, typesPerDay.tail, ((typesPerDay.head._1, elementSelected) :: lunchesSelected))
-      }
-    }
-
-    dishesForAWeek(lunches, typesPerDay, List())
-  }
-}
-
-
-
