b0ace07d0c80a1e3f97c6ba02b39450b26009527
Alberto Rodriguez <arodriguez@stratio.com>
2017-02-28 09:19:55 +0100

    Move generate template code to its own class

--- src/main/scala/org/ardlema/executor/Executor.scala
+++ src/main/scala/org/ardlema/executor/Executor.scala
@@ -1,12 +1,9 @@
 package org.ardlema.executor
 
-import java.io.{File, StringWriter}
-import java.util.{ArrayList, HashMap}
+import java.io.File
 
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.velocity.VelocityContext
-import org.apache.velocity.app.VelocityEngine
-import org.ardlema.mailer.MenuMailer
+import org.ardlema.mailer.{MenuMailer, TemplateGenerator}
 import org.ardlema.parser.{CommandLineParser, DishParserFromTextFile}
 import org.ardlema.planner.MenuPlanner
 
@@ -17,42 +14,18 @@ object Executor extends LazyLogging {
     logger.info("Parsing command line parameters")
     val commandLineParams = CommandLineParser.parse(args)
     if (commandLineParams.isDefined) {
+      logger.info("Command line parameters parsed fine")
       logger.info("Starting the menu planner execution...")
-      //TODO: Fix me!! Pick the path from a command line parameter!!!
-      val lunchesFilePath = "/home/arodriguez/dev/menu-planner/src/main/resources/lunches.txt"
-      val dinnersFilePath = "/home/arodriguez/dev/menu-planner/src/main/resources/dinners.txt"
-      val ve = new VelocityEngine()
-      ve.init()
+      val lunchesFilePath = s"""${commandLineParams.get.rootPath}${DishParserFromTextFile.lunchesTextFile}"""
+      val dinnersFilePath = s"""${commandLineParams.get.rootPath}${DishParserFromTextFile.dinnersTextFile}"""
       val lunches = DishParserFromTextFile.parse(new File(lunchesFilePath))
       val dinners = DishParserFromTextFile.parse(new File(dinnersFilePath))
       val plannedLunches = MenuPlanner.planAWeek(lunches, LunchesPerDay.lunchesPerDay)
       val plannedDinners = MenuPlanner.planAWeek(dinners, LunchesPerDay.dinnersPerDay)
-      val context = new VelocityContext()
-      val lunchesList = new ArrayList[HashMap[String, String]]()
-      val dinnersList = new ArrayList[HashMap[String, String]]()
-      context.put("lunchesList", lunchesList)
-      context.put("dinnersList", dinnersList)
+      logger.info("Generating email template...")
+      val mailBody = TemplateGenerator.generateTemplate(plannedLunches, plannedDinners)
 
-      for (lunch <- plannedLunches) {
-        val lunchesMap = new HashMap[String, String]()
-        lunchesMap.put("description", lunch._2.description)
-        lunchesMap.put("ingredients", lunch._2.ingredients)
-        lunchesList.add(lunchesMap)
-      }
-
-      for (dinner <- plannedDinners) {
-        val dinnersMap = new HashMap[String, String]()
-        dinnersMap.put("description", dinner._2.description)
-        dinnersMap.put("ingredients", dinner._2.ingredients)
-        dinnersList.add(dinnersMap)
-      }
-
-      val writer = new StringWriter()
-      val template = ve.getTemplate("/src/main/resources/niceemail.vm")
-      template.merge(context, writer)
-      val body = writer.toString
-
-      MenuMailer.sendMessage(body, commandLineParams.get)
+      MenuMailer.sendMessage(mailBody, commandLineParams.get)
     } else {
       logger.error("Wrong command line parameters. Please review the list of required params!!")
     }
